75fd417792de2704f6db34eda7d3986452cf74a2
talithafawwaz <talithafawwaz07@gmail.com>
2023-05-12 12:20:48 +0700

    delete redux

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import coursesSlice from "./Courses/coursesSlice";
-
-// export const store = configureStore ({
-//     reducer: {
-//         courses: coursesSlice
-//     }
-// })
